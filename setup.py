37b1bb30fb1f397102b578697518d349d0900035
Alex Flint <alex.flint@gmail.com>
2015-07-02 11:31:35 -0700

    update url in setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from distutils.core import setup
 
 setup(name='triangulation',
       description='3D triangulation algorithms',
-      version='0.1',
+      version='0.11',
       author='Alex Flint',
       author_email='alex.flint@gmail.com',
-      url='...',
+      url='https://github.com/alexflint/triangulation',
       packages=['triangulation'],
       package_dir={'triangulation': '.'},
       scripts=['cmds/evaluate_algorithm', 'cmds/triangulate_dataset']
