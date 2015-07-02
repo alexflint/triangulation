735c17888f8594a6618d05f83501384505c18ba8
Alex Flint <alex.flint@gmail.com>
2015-07-01 20:58:45 -0700

    remove "calibrated" package, move everything up to "triangulation"

--- __init__.py
+++ __init__.py
@@ -1,1 +1,16 @@
-from .io import load_matlab_dataset, load_vgg_dataset, Observation, Camera, Bundle
+from .triangulate import \
+	algorithms, \
+	triangulate, \
+	triangulate_midpoint, \
+	triangulate_linear, \
+	triangulate_directional, \
+	triangulate_directional_pair, \
+	triangulate_infnorm, \
+	triangulate_infnorm_fixed \
+
+from .io import \
+	load_matlab_dataset, \
+	load_vgg_dataset, \
+	Observation, \
+	Camera, \
+	Bundle
