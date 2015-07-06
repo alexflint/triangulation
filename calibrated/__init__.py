8b26a8b9b42616f112042f9b71d08c3316e051e5
Alex Flint <alex.flint@gmail.com>
2015-07-05 19:34:03 -0700

    remove calibrated directory

--- calibrated/__init__.py
+++
@@ -1,9 +0,0 @@
-from .triangulate import \
-	algorithms, \
-	triangulate, \
-	triangulate_midpoint, \
-	triangulate_linear, \
-	triangulate_directional, \
-	triangulate_directional_pair, \
-	triangulate_infnorm, \
-	triangulate_infnorm_fixed \
